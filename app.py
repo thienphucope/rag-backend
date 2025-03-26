bedd4d05db307736345d957b9df86f5353a39f85
ADMIN\HPVictus <thienphuc1052004@gmail.com>
2025-03-26 20:05:29 +0700

    fixx sum prompt

--- app.py
+++ app.py
@@ -128,7 +128,7 @@ def summarize_and_store(username):
     prompt = f"""
     "Summarize the information about user in a '{username}, attribute : value' format in a single paragraph separated with a semicolon ';' . If it is {username} question, ignore and don't summarize it. Capture separated key facts. Follow the format, do not use asterisks, all in lowercase"
     
-    Example: Ope,name = Ope Watson; ope, age, how old, birth year = 20
+    Example: {username},height = 20 ; {username}, age, how old, birth year = 20
 
     Conversation history:
     {conversation_text}
